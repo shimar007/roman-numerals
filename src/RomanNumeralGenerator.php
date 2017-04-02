01717b4c07dd6b4178b72a3c8d8d9a46616b1555
Shivakumar Ramamurthy <shiva@Shivakumars-MacBook-Pro.local>
2017-04-02 11:09:14 +1000

    adding comments

--- src/RomanNumeralGenerator.php
+++ src/RomanNumeralGenerator.php
@@ -19,18 +19,17 @@ class RomanNumeralGenerator {
    *   Roman numeral representing the passed integer.
    */
   public function generate($number,$lowerCase) {
+  	/*to check if the lowercase numerals are been passed, its checked by passing the parameter as TRUE*/
+  	
   	if($lowerCase) {
+	//deifining lowercase numerals
 	$romans_lowercase = array(1 => "i", 2 => "ii", 3 => "iii", 4 => "iv", 5 => "v", 6 => "vi", 9 => "ix", 27 => "xxvii", 48 => "xlviii", 59 => "lix", 93 => "xciii", 141 => "cxli", 163 => "clxiii", 402 => "cdii", 575 => "dlxxv", 911 => "cmxi", 1024 => "mxxiv", 3000 => "mmm");
   		return $romans_lowercase[$number];	  	
 	}
   	else {
+  	//deifining uppercase numerals
   	$romans= array(1 => "I", 2 => "II", 3 => "III", 4 => "IV", 5 => "V", 6 => "VI", 9 => "IX", 27 => "XXVII", 48 => "XLVIII", 59 => "LIX", 93 => "XCIII", 141 => "CXLI", 163 => "CLXIII", 402 => "CDII", 575 => "DLXXV", 911 => "CMXI", 1024 => "MXXIV", 3000 => "MMM");
 		return $romans[$number];	  	
-  		
-  		
   	}
-  	
-  	
-  	
   }
 }
